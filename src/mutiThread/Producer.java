9483bae1563674e5c346ebb16e4e3e5046a4dc8e
root <1948976637@qq.com>
2019-03-01 17:55:16 +0800

    volatile关键字

--- src/mutiThread/Producer.java
+++ src/mutiThread/Producer.java
@@ -15,7 +15,7 @@ public class Producer implements Runnable{
    public void run() {
        try {
            while (true){
-               Thread.sleep(1000);//模拟耗时
+               Thread.sleep(500);//模拟耗时
                queue.put(produce());
            }
        }catch (InterruptedException e){
@@ -25,7 +25,7 @@ public class Producer implements Runnable{
  
    private int produce() {
       // int n=new Random().nextInt(10000);
-       int n = 10;
+       int n = 1;
        System.out.println("Thread:" + Thread.currentThread().getId() + " produce:" + n);
        return n;
    }
